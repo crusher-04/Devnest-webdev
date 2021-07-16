a73dc40e15d6cdf7def6859c36fd69b72b4bb155
crusher-04 <83916750+crusher-04@users.noreply.github.com>
2021-07-16 20:20:16 +0530

    Create index.js

---
+++ day11/index.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+      numb: 1,
+      question: "What is the capital of Uttar Pradesh",
+      answer: "Lucknow",
+      options: [
+        "Kanpur",
+        "Lucknow",
+        "Delhi",
+        "gorakhpur",
+      ],
+    },
+    {
+      numb: 2,
+      question: "How many states are there in India?",
+      answer: "29",
+      options: [
+        "27",
+        "28",
+        "29",
+        "30",
+      ],
+    },
+    {
+      numb: 3,
+      question: "What is the tenure of Prime Mininster in India",
+      answer: "5",
+      options: [
+        "16",
+        "10",
+        "3",
+        "5",
+      ],
+    },
+    {
+      numb: 4,
+      question: "How many Union Territories are there in India",
+      answer: "8",
+      options: [
+        "6",
+        "7",
+        "8",
+        "9",
+      ],
+    },
+    {
+      numb: 5,
+      question: "Biggest Ocean in the world",
+      answer: "Pacific Ocean",
+      options: [
+        "Pacific Ocean",
+        "Indian Ocean",
+        "Atlantic Ocean",
+        "Arctic Ocean",
+      ],
+    },
+  ];
