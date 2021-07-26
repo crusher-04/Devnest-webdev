0fb268c8c6979ea7509f66c13cc343777e67a35d
crusher-04 <83916750+crusher-04@users.noreply.github.com>
2021-07-26 20:05:08 +0530

    Create script.js

---
+++ day13/script.js
@@ -0,0 +1,17 @@
+const quoteText = document.getElementById("quote-text"),
+  quoteAuthor = document.getElementById("quote-author"),
+  genQuoteBtn = document.getElementById("gen-new-quote");
+
+function randomQuote() {
+  fetch("https://api.quotable.io/random")
+    .then((res) => res.json())
+    .then((data) => {
+      quoteText.textContent = data.content;
+      quoteAuthor.textContent = `-- ${data.author}`;
+    });
+}
+
+randomQuote();
+genQuoteBtn.addEventListener("click", () => {
+  randomQuote();
+});
